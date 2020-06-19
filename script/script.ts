2348aa7aaffa48bfd813a3bb9c15d0e9e2807d8e
Kathrin Schey <kathrin.dostal@gmail.com>
2020-06-19 17:32:59 +0200

    some margins try and commitit

--- script/script.ts
+++ script/script.ts
@@ -1,9 +1,11 @@
 
-//let teaserPic = [];
+$(document).ready(function(){
+//let for the classes
 let lokArray = [];
 let restArray = [];
 let eventArray = [];
 
+//class lokation start
 class lokation
 {
 	pic: string = "";
@@ -22,18 +24,21 @@ class lokation
 	display()
 	{
 		return `
-					<div class='col-lg-3 col-md-6 col-sm-12 display'>
-						<img class='img-fluid' src='${this.pic}'>
+					<div class='col-lg-3 col-md-6 col-sm-12 inhalt border'>
+						<div class='d-none d-sm-block'>
+							<img class='img-fluid rounded' src='${this.pic}'>
+						</div>
 						<h3>${this.adr}</h3>
 						<p>${this.city}</p>
-					</div>
-				`					;
+					</div>`;
 	}
 }
-
+//class lokation extends
+//input for the array/class lokation start
 let church = new lokation(`img/pic1.jpg`, `St. Charles Church`, `Karlsplatz 1, 1010 Vienna`);
 let park = new lokation(`img/pic2.jpg`,`Schönbrunn Park, Vienna`, `Maxingstraße 13b, 1130 Vienna`);
-
+//input for the array/class lokation ends
+//class restaurant start
 class restaurants extends lokation 
 {
 	telNum : number;
@@ -52,22 +57,24 @@ class restaurants extends lokation
 
 	display()
 	{
-		return `
-					<div class='col-lg-3 col-md-6 col-sm-12 display'>
-						<img class='img-fluid' src='${this.pic}'>
+		return 	   `<div class='col-lg-3 col-md-6 col-sm-12 inhalt border'>
+						<div class='d-none d-sm-block'>
+							<img class='img-fluid rounded' src='${this.pic}'>
+						</div>
 						<h3>${this.adr}</h3>
 						<p>${this.city}</p>
 						<p>${this.telNum}</p>
 						<p>${this.nation}</p>
 						<p>${this.url}</p>
-					<div>
-				`;
+					<div>`;
 	}
 }
-
+//class restaurant ends
+//input for array/class restaurant start
 let steak = new restaurants(`img/pic3.jpg`, `Dstrikt Steakhouse`, `Schubertring 5-7, 1010 Vienna`, `0043-131188616`, `Bürgerlich`, `www.dstrikt.com`);
 let fish = new restaurants(`img/pic4.jpg`, `Kulinarium 7`, `Sigmundsgasse 1,1070 Vienna`, `0043-5220530`, `National`, `https://www.kulinarium7.com/`);
-
+//input for array/class restaurant ends
+//class event start
 class events extends lokation
 {
 	date : number;
@@ -85,27 +92,29 @@ class events extends lokation
 	
 	display()
 	{
-		return `
-					<div class='col-lg-3 col-md-6 col-sm-12 display'>
-						<div>
-							<img class='img-fluid' src='${this.pic}'>
-						</div>
+		return 		`<div class='col-lg-3 col-md-6 col-sm-12 inhalt border'>
+						<div class='d-none d-sm-block'>						
+							<img class='img-fluid rounded' src='${this.pic}'>
+						</div>						
 						<h3>${this.adr}</h3>
 						<p>${this.city}</p>
 						<p>${this.date}</p>
 						<p>${this.time}</p>
 						<p>${this.price}</p>
-					<div>
-				`;
+					<div>`;
 	}
 }
-
+//class event ends
+//input for array/class event start
 let dropout = new events(`img/pic5.jpg`, `Seasik Steve by Arena`, `Baumgasse 80, 1030 Wien`, `30.05.2020`, `20:00`, `40€`);
 let blues = new events(`img/pic6.jpg`, `Joe Bonamassa: An Acoustic Evening At The Vienna Opera`, `Opernring 2, 1010 Wien`, `30.10.2020`, `20:00`, `80€`);
+//input for array/class event ends
 
+//loop for print
 for (let i = 0; i < lokArray.length; i++)
 {
 	document.getElementById("result").innerHTML += lokArray[i].display();
 			
 }
 
+})
